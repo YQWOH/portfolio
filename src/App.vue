be7cec1b6325ad26f7c11bc531a1751c3c836c83
YQWOH <93274829+YQWOH@users.noreply.github.com>
2023-06-23 01:01:16 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,280 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+ <header class="header">
+
+  <div class="logo__container">
+      <a href="/">
+          <svg xmlns="http://www.w3.org/2000/svg" class="logo__image" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+      </a>
+  </div>
+
+  <nav class="navigation__container">
+      <ol class="navigation__links">
+          <li class="link__item">
+              <span class="highlight"> 01. </span>
+              <a class="link__name underline" href="/#about"> About </a>
+          </li>
+          <li class="link__item">
+              <span class="highlight">02.</span>
+              <a class="link__name underline" href="/#experience"> Experience </a>
+          </li>
+          <li class="link__item">
+              <span class="highlight">03.</span>
+              <a class="link__name underline" href="/#work"> Work </a>
+          </li>
+          <li class="link__item">
+              <span class="highlight">04.</span>
+              <a class="link__name underline" href="/#contact"> Contact </a>
+          </li>
+      </ol>
+  </nav>
+
+  <button class="mobile__navigation">
+      <svg class="menu__icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
+      </svg>
+  </button>
+  </header>
+
+  <main>
+  <section id="home" class="home__section">
+      <h4 class="home__title">Hi, my name is</h4>
+      <h2 class="home__username">Steven Woh.</h2>
+      <h3 class="home__subtitle">I build things for the web.</h3>
+      <p class="home__description">
+          Empowering individuals to become
+          <span class="highlight">self-taught developers</span>
+          and solving problems through continuous learning and product development drives my passion. My ultimate objective is to become a 
+          <span class="highlight">full-stack developer</span>
+          and inspire others to embark on this rewarding journey.
+      </p>
+      <div class="home__cta">
+          <a class="cta__link" href="/#contact"> Contact me! </a>
+      </div>
+  </section>
+
+  <section id="about" class="about__section">
+          <h2 class="about__title">
+              <span class=""> 01. </span>
+              About Me
+          </h2>
+          <div class="about__container">
+              <div class="about__content">
+                  <div class="about__content--text">
+                      <p>
+                          Hello! My name is
+                          <span class="highlight">Steven Woh</span>.
+                          Driven by a thirst for knowledge and a passion for technology, I embarked on a journey of web development in 2021, which led me to the exciting world of
+                          <span class="highlight">Web Development</span>.
+                      </p>
+                      <p>
+                          As a developer, my primary focus is on
+                          <span class="highlight"> creating accessible </span> and <span class="highlight">inclusive products that cater to diverse audiences</span>.
+                      </p>
+                      <p>Here are a few technologies I’ve been working with recently:</p>
+                  </div>
+                  <ul class="about__technologies">
+                      <li>JavaScript</li>
+                      <li>TypeScript</li>
+                      <li>Vue</li>
+                      <li>Node.js</li>
+                      <li>TailwindCSS</li>
+                      <li>React</li>
+                      <li>Quasar</li>
+                      <li>My SQL</li>
+                  </ul>
+              </div>
+              <div class="about__image">
+                  <img src="./assets/profile.jpeg" alt="profile" />
+              </div>
+          </div>
+  </section>
+
+  <section id="experience" class="experience__section">
+      <h2 class="title">
+          <span class="highlight"> 02. </span>Where I've Worked
+      </h2>
+      <div class="cards">
+          <div>
+              <h3>
+                  <span>Web Developer</span>
+                  <span>
+                      @
+                      <a class="link-underline" href="#"> Willowglen Sdn Bhd </a>
+                  </span>
+              </h3>
+              <p>July 2022 - Present</p>
+              <div>
+                  <ul>
+                      <li>Developing and maintaining web application using languages such as Typescript, Quasar & Vue Js for Front End, Express & Node Js for backend.</li>
+                      <li>Creating and integrating web applications and features to improve user experience.</li>
+                      <li>Collaborating with designers, content creators, and other developers to ensure website functionality and design align.</li>
+                      <li>Writing well-designed, testable and efficient code.</li>
+                  </ul>
+              </div>
+          </div>
+          <div>
+              <h3>
+                  <span>SAP Executive</span>
+                  <span >
+                      @
+                      <a class="link-underline" href="#"> Top Glove Sdn Bhd </a>
+                  </span>
+              </h3>
+              <p>July 2019 - June 2022</p>
+              <div>
+                  <ul>
+                      <li>Perform SAP configuration, check and upload master data, unit testing, System Integrating testing, conduct UAT with user</li>
+                      <li>Support user on IT part, liaise with different IT team to ensure project can be deliver on time and fulfil with the requirement</li>
+                      <li>Handle change request and enhancement for FI and CO module.</li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+  </section>
+
+  <section id="work" class="work__section">
+      <h2 class="work__title">
+          <span> 03. </span>
+          Some Things I've Built
+      </h2>
+      <ul class="work__cards">
+          <li class="work__card">
+              <div class="project__container">
+                  <p>Featured Project</p>
+                  <h3 class="project__title">
+                      <a href="#">Budget App</a>
+                  </h3>
+                  <div class="project__description">
+                      <p>A budget application that can calculate the income and expense amount.</p>
+                  </div>
+                  <ul class="project__technologies">
+                      <li>Vue Js</li>
+                      <li>TailwindCSS</li>
+                  </ul>
+                  <div class="project__links">
+                      <a class="z-10" target="_blank" href="https://master--friendly-meerkat-0fa5ac.netlify.app/">
+                          website
+                      </a>
+                  </div>
+              </div>
+            
+              <div class="project__image">
+                  <img  src="./assets/budget.jpeg" alt="" />
+              </div>
+          </li>
+
+          <li class="work__card">
+              <div class="project__container">
+                  <p>Featured Project</p>
+                  <h3 class="project__title">
+                      <a href="#">Weather Application</a>
+                  </h3>
+                  <div class="project__description">
+                      <p>A weather application that able to show the selected city weather.</p>
+                  </div>
+                  <ul class="project__technologies">
+                      <li>Vue Js</li>
+                      <li>TailwindCSS</li>
+                      <li>Pinia</li>
+                  </ul>
+                  <div class="project__links">
+                      <a class="z-10" target="_blank" href="https://celadon-ganache-136900.netlify.app/">
+                          website
+                      </a>
+                  </div>
+              </div>
+
+              <div class="project__image">
+                  <img src="https://i0.wp.com/palawan-news.com/wp-content/uploads/2021/01/Weather-01.jpg?fit=496%2C331&ssl=1" alt="" />
+              </div>
+          </li>
+      </ul>
+  </section>
+
+  <section id="contact" class="contact__section">
+      <h2 class="contact__title">
+          <span> 04. </span>
+          What's Next?
+      </h2>
+      <div class="contact__container">
+          <div class="contact__content">
+              <h3>Get In Touch</h3>
+              <p>Although I’m not currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!</p>
+          </div>
+          <div class="contact__cta">
+              <a href="#"> Say Hello </a>
+          </div>
+      </div>
+  </section>
+  </main>
+
+  <footer class="footer__container">
+  <ul class="footer__socials">
+      <li class="social__item">
+          <a class="social__link" href="https://wa.me/60179565828" target="_blank">
+              <svg class="social__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                  <path
+                      d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"
+                  ></path>
+              </svg>
+          </a>
+      </li>
+
+      <li class="social__item">
+          <a class="social__link" href="https://github.com/YQWOH" target="_blank">
+              <svg class="social__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <path d="M12 0a12 12 0 00-3.798 23.355c.6.111.822-.258.822-.576 0-.284-.01-1.033-.015-2.025-3.338.723-4.042-1.611-4.042-1.611-.546-1.395-1.333-1.764-1.333-1.764-1.089-.745.084-.729.084-.729 1.205.084 1.839 1.238 1.839 1.238 1.07 1.836 2.807 1.305 3.493.998.109-.776.418-1.305.762-1.605-2.665-.302-5.466-1.332-5.466-5.931 0-1.311.465-2.381 1.233-3.219-.123-.302-.534-1.523.116-3.176 0 0 1.008-.322 3.3 1.23a11.428 11.428 0 016.066 0c2.29-1.552 3.297-1.23 3.297-1.23.65 1.653.24 2.874.117 3.176.77.838 1.232 1.908 1.232 3.219 0 4.611-2.803 5.625-5.477 5.919.431.372.818 1.104.818 2.223 0 1.605-.015 2.898-.015 3.292 0 .318.216.693.828.576A11.996 11.996 0 0024 12.001C24 5.372 18.627 0 12 0z" />
+              </svg>
+          </a>
+      </li>
+
+      <li class="social__item">
+          <a class="social__link" href="https://www.linkedin.com/in/yn-qin-woh-394ab0183/" target="_blank">
+              <svg class="social__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                  <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path>
+              </svg>
+          </a>
+      </li>
+  </ul>
+
+  <div>
+      <small>Designed & Built by Steven Woh</small>
+  </div>
+  </footer>   
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
